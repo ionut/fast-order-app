682471845e79271c7845f0303bb2ad4a6789b18a
Husoschi Ionut-Catalin <ionuthusoschi@gmail.com>
2024-03-19 23:49:26 +0200

    clear cart feature

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,13 +1,18 @@
-import { Link } from 'react-router-dom';
+
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart, getCart } from './cartSlice';
+import { getUsername } from '../user/userSlice';
+import EmptyCart from './EmptyCart'
 
 function Cart() {
-  const username = useSelector(state => state.user.username);
-  const cart = useSelector(state => state.cart.cart)
+  const username = useSelector(getUsername);
+  const cart = useSelector(getCart);
+  const dispatch = useDispatch();
+
+  if (!cart.length) return <EmptyCart />
 
   return (
     <div className="px-4 py-3">
@@ -26,7 +31,7 @@ function Cart() {
           Order pizzas
         </Button>
 
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={() => dispatch(clearCart())}>Clear cart</Button>
       </div>
     </div>
   );
